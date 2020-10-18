0933bc8a54e861ccb64968fdff75120e1cf125ff
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-18 17:00:26 -0400

    starter mix

---
+++ homework/gamePlan.js
@@ -0,0 +1,7 @@
+//via DOM, target destination
+    //first attach the 'thing'/'value' I want to append/prepend
+//make use of API:
+    //API key
+    //url/method:desired API/'GET
+//create from scratch!
+
