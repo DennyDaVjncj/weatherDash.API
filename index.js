5147d16756e1e1696ffc3fe08e09f263b47bc0ee
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-19 12:17:16 -0400

    updated app

---
+++ index.js
@@ -0,0 +1,4 @@
+//target needed elements
+let domUserQuery=$("#userQuery");//trgt user query
+let searchBttn=$("#clckEvnt1");//triggers request to api, then prepends query to page
+let storedQuery=$("#prePend");//element I wish to multiply on click event
