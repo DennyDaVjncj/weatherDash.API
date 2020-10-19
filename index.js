b25e072867798bcb3d27a400c6ad5c8f930d6341
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-19 15:04:41 -0400

    linked index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //target needed elements
-let domUserQuery=$("#userQuery");//trgt user query
+let userQuery=$("#userQuery");//trgt user query
 let searchBttn=$("#clckEvnt1");//triggers request to api, then prepends query to page
 let storedQuery=$("#prePend");//element I wish to multiply on click event
+
+function userQ(){
+    let captureQuery=userQuery.val();
+    console.log(captureQuery);
+}userQ();
