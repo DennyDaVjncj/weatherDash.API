8d22cbb7f4b5b240d50c31b39e57576c60a5d603
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-19 02:21:51 -0400

    client structure complete

--- vitamins/homework/gamePlan.js
+++
@@ -1,7 +0,0 @@
-//via DOM, target destination
-    //first attach the 'thing'/'value' I want to append/prepend
-//make use of API:
-    //API key
-    //url/method:desired API/'GET
-//create from scratch!
-
