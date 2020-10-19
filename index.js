7ffde8465535fba1ae1b279049e94a6d9fbf36a7
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-19 15:06:33 -0400

    new logic direction

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 let userQuery=$("#userQuery");//trgt user query
 let searchBttn=$("#clckEvnt1");//triggers request to api, then prepends query to page
 let storedQuery=$("#prePend");//element I wish to multiply on click event
+let domDiv=$("<div>");//new divs for every user temp search
+let captureQuery=userQuery.val();//captures user input via DOM
 
-function userQ(){
-    let captureQuery=userQuery.val();
-    console.log(captureQuery);
-}userQ();
+function getRequest(){
+    //create what I need on the client
+        //this may be the new direction
+}
+
+//focusing on stored query selections, I will populate <divs> on user request(s).
+    //pass to localStorage as well
