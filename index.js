2435d82b81f11ad738eab5aff16d1587eabbcb01
Denny da Vjncj <daVjncjCode@gmail.com>
2020-10-21 19:14:58 -0400

    version control

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 //target needed elements
-let userQuery=$("#userQuery").val();//trgt user query
+let userQuery=$("#userQuery").val().trim();//trgt user query
 let searchBttn=$("#clckEvnt1");//triggers request to api, then prepends query to page
 let storedQuery=$("#prePend");//element I wish to multiply on click event
 let domDiv=$("<div>");//new divs for every user temp search
 let pendingList=$("#prePend");//trgtng div which will store newDivs
 
-function initialQuery(){
-    //create what I need on the client via DOM
-        //this may be the new direction
-    searchBttn.on("click",function(){
-        domDiv.text(userQuery);
-        pendingList.append(domDiv);
-    })
-}
 
+    domDiv.text(userQuery).append(domDiv);   
+    
+    searchBttn.on("click",function(e){
+        e.preventDefault();
+        pendingList.append(domDiv);
+        initialQuery;
+    })
 
 //focusing on stored query selections, I will populate <divs> on user request(s).
     //pass to localStorage as well
